a2bdb6c582a80ebca18c5be1c6882d2c2a263b8c
sameerhashout <sameerkumar@hashouttech.com>
2025-02-19 22:48:41 +0530

    main commit changes 2

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,78 +1,52 @@
-import { fetchPlaceholders, getMetadata } from "../../scripts/aem.js";
-const placeholders = await fetchPlaceholders(getMetadata("locale"));
+import { fetchPlaceholders, getMetadata } from '../../scripts/aem.js';
 
+const placeholders = await fetchPlaceholders(getMetadata('locale'));
 const { btnNxt, btnPre } = placeholders;
+
 export default function decorate(block) {
-  console.log("placeholders ---> ", placeholders, btnNxt, btnPre);
   const rows = [...block.children];
-  [...block.children].forEach((row, r) => {
-    if (r == 0) {
-      const nextbtn = document.createElement("button");
-      nextbtn.classList.add("btn");
-      nextbtn.classList.add("btn-next");
+  rows.forEach((row, r) => {
+    if (r === 0) {
+      const nextbtn = document.createElement('button');
+      nextbtn.classList.add('btn', 'btn-next');
       const node = document.createTextNode(btnNxt);
       nextbtn.append(node);
       row.replaceWith(nextbtn);
-    } else if (r == rows.length - 1) {
-      const prebtn = document.createElement("button");
-      prebtn.classList.add("btn");
-      prebtn.classList.add("btn-prev");
+    } else if (r === rows.length - 1) {
+      const prebtn = document.createElement('button');
+      prebtn.classList.add('btn', 'btn-prev');
       const node = document.createTextNode(btnPre);
       prebtn.append(node);
       row.replaceWith(prebtn);
     } else {
-      row.classList.add("slide");
+      row.classList.add('slide');
       [...row.children].forEach((col, c) => {
-        if (c == 1) {
-          col.classList.add("slide-text");
+        if (c === 1) {
+          col.classList.add('slide-text');
         }
       });
     }
   });
 
-  const slides = document.querySelectorAll(".slide");
-
-  // loop through slides and set each slides translateX
+  const slides = document.querySelectorAll('.slide');
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${indx * 100}%)`;
   });
 
-  // select next slide button
-  const nextSlide = document.querySelector(".btn-next");
-
-  // current slide counter
+  const nextSlide = document.querySelector('.btn-next');
   let curSlide = 0;
-  // maximum number of slides
-  let maxSlide = slides.length - 1;
+  const maxSlide = slides.length - 1;
 
-  // add event listener and navigation functionality
-  nextSlide.addEventListener("click", function () {
-    // check if current slide is the last and reset current slide
-    if (curSlide === maxSlide) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-
-    //   move slide by -100%
+  nextSlide.addEventListener('click', () => {
+    curSlide = curSlide === maxSlide ? 0 : curSlide + 1;
     slides.forEach((slide, indx) => {
       slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
   });
 
-  // select next slide button
-  const prevSlide = document.querySelector(".btn-prev");
-
-  // add event listener and navigation functionality
-  prevSlide.addEventListener("click", function () {
-    // check if current slide is the first and reset current slide to last
-    if (curSlide === 0) {
-      curSlide = maxSlide;
-    } else {
-      curSlide--;
-    }
-
-    //   move slide by 100%
+  const prevSlide = document.querySelector('.btn-prev');
+  prevSlide.addEventListener('click', () => {
+    curSlide = curSlide === 0 ? maxSlide : curSlide - 1;
     slides.forEach((slide, indx) => {
       slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
